f5bc530bf716f1c9e7cca58baa774a1ef011cf66
Ritesh Kumar <ritesh@mailmodo.com>
2023-01-14 08:59:43 +0000

    black Out

--- src/App.js
+++ src/App.js
@@ -3,46 +3,51 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h2 style={{ textAlign: "center" }}>
-        Create a contact management API service
-      </h2>
+      <h2 style={{ textAlign: "center" }}>Blackout Period!</h2>
       <p>
-        A contact is a real world person, with or without an email id. The
-        contact can also have arbitrary properties. The aim of this excersize is
-        to help a platfomr save a list of contacts of their client which they
-        can use for future segemnetation, marking and other stuff.
+        A user wants to schedule emails based on a regular interval, say 100
+        emails every hour from a big list of 10K Emails. But he doesn't want to
+        send them at night. One of the teams in tech has implemented the
+        scheduler but forgot to implement this blackout. Your task would be to
+        create a function which takes three parameters startTime, endTime and
+        current Time, And returns true or false based on If current time lies in
+        the blackout period. some examples{" "}
       </p>
-      <p>
-        We need to be aware of the Spammers and so, we need to put some rate
-        limits. To make it easy we generally accept any data and allow clients
-        to update them at later stage or merge it another contact. And we need
-        to trace all the changes in the data as well. Anything not specified
-        below can be assumed to solve the problem.
-      </p>
-      <p>
-        You need the Show a running code where data can be added/updaed/merged
-        and filters to output as json from Postman. Its Ok to keep data in
-        memory if it speeds up the development process. Keep in mind that you
-        only have 30-40 minutes (max) to show your approach and a working
-        application <br />
-        <br />
-        <b>Primary identification key</b>: Email id, if present, User can
-        provide the contact_id, Otherwise the contact is assigned an auto
-        generated contact_id
-      </p>
+< table width="100%" border="1">
+  <tr>
+    <th>startTime</th>
+    <th>endTime</th>
+    <th>currentTime</th>
+  </tr>
+  <tr>
+    <td>22:00</td>
+    <td>18:00</td>
+    <td>23:00</td>
+    <td>True</td>
+  </tr>
+  <tr>
+    <td>18:00</td>
+    <td>22:00</td>
+    <td>23:00</td>
+    <td>False</td>
+  </tr>
+  <tr>
+    <td>18:00</td>
+    <td>22:00</td>
+    <td>231:00</td>
+    <td>True</td>
+  </tr>
+</table>
 
-      <strong>Create the following api</strong>
+      <strong>some assumptions</strong>
       <ol>
-        <li>Add or update contact</li>
-        <li>Merge contacts on the basis primary identification</li>
-        <li>Update email id on the basis of contact_id</li>
-        <li>Filter contacts on the basis of properties</li>
-      </ol>
-      <strong>Additionally</strong>
-      <ol>
-        <li>Keep an audit log of add/update/merge action </li>
         <li>
-          Add a rate limiter on add update operation for 2 requests /10 seconds
+          Time can only be expressed in HH:MM and Seconds would not be
+          considered.
+        </li>
+        <li>
+          There is no Date parameter, So if it starts with 18 and ends with 9.
+          Then 9 is actually 9 am for the next date.
         </li>
       </ol>
     </div>
